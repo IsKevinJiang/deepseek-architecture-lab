19394bfce381e77493532a91fd4b4d2bb5190bfb
Kevin Jiang <kevinj7752@gmail.com>
2026-07-21 23:03:54 -0400

    implemented sharding with FineWeb dataset streaming

--- deepseek_lab/normalization.py
+++ deepseek_lab/normalization.py
@@ -3,10 +3,10 @@ import torch
 import torch.nn as nn
 
 class RMSNorm(nn.Module):
-    def __init__(self, hidden_size, eps = 1e-6) -> None:
+    def __init__(self, hidden_dim, eps = 1e-6) -> None:
         super().__init__()
         self.eps = eps
-        self.weight = nn.parameter.Parameter(torch.ones(hidden_size))
+        self.weight = nn.parameter.Parameter(torch.ones(hidden_dim))
 
     def forward(self, x):
         input_type = x.dtype
--- scripts/prepare_dataset.py
+++ scripts/prepare_dataset.py
@@ -1,9 +1,12 @@
 import tiktoken
 import numpy as np
 from deepseek_lab.data import ShardWriter
+from datasets import load_dataset
+from tqdm import tqdm
+
+enc = tiktoken.get_encoding("gpt2")
 
 def tokenize_document(text):
-    enc = tiktoken.get_encoding("gpt2")
     tokens = [enc.eot_token]
     tokens.extend(enc.encode_ordinary(text))
 
@@ -13,11 +16,14 @@ def tokenize_document(text):
 
     return tokens
 
+#Preparing the fineweb-edu dataset(Stops after 100 documents)
 def main():
-    text = "Modern Watermelons originated in Sudan, where it was first cultivated.[2][3] Kordofan melons from Sudan are the closest relatives and may be progenitors of modern, cultivated watermelons.[4] Wild watermelon seeds were found in Uan Muhuggiag, a prehistoric site in Libya that dates to approximately 3500 BC.[5] In 2022, a study was released that traced 6,000-year-old watermelon seeds found in the Libyan desert to the Egusi seeds of Nigeria, West Africa.[6] Watermelons were domesticated in Sudan and cultivated in Egypt by 2000 BC; an image of an apparent one on a tray has been found in an Egyptian tomb dating at least to 4000 years ago. Those were not of the sweet modern variety, but Hebrew texts from early in the Christian era place watermelon with other sweet table fruits, and dessert watermelons spread across the Mediterranean world during Roman antiquity.[7]"
-    shard_writer = ShardWriter(10, "data")
-    tokens = tokenize_document(text)
-    shard_writer.add(tokens)
+    dataset = load_dataset("HuggingFaceFW/fineweb-edu", "sample-10BT", split="train", streaming=True)
+    shard_writer = ShardWriter(10_000, "data/fineweb_smoke")
+    documents = dataset.take(100)
+    for document in tqdm(documents, total=100):
+        tokens = tokenize_document(document["text"])
+        shard_writer.add(tokens)
     shard_writer.finish()
 
 if __name__ == "__main__":
